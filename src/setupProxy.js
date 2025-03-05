95a5e41e90877c9920a1d8a1b06cecb3c427ee26
Daniil Fomenko <danil.fomchik@gmail.com>
2025-03-05 14:36:00 +0200

    format code style

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,11 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const {createProxyMiddleware} = require('http-proxy-middleware');
 
 module.exports = function (app) {
     app.use(
-        "/api/",
+        '/api/',
         createProxyMiddleware({
-            target: "https://rickandmortyapi.com/api",
+            target: 'https://rickandmortyapi.com/api',
             changeOrigin: true,
-        })
+        }),
     );
 };
